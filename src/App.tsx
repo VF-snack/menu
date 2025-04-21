89b772e842707d29f4c2509b5b8d5d314e8f7352
VF-snack <vfsnack8@gmail.com>
2025-04-21 21:01:11 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from "./screens/Menu/Menu";
 
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Routes>
           <Route path="/" element={<Menu />} />
         </Routes>
